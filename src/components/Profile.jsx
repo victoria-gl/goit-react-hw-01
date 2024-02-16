bdbf8b7d67e2c21065f5626466bf35565e95b93b
victoria-gl <138527509+victoria-gl@users.noreply.github.com>
2024-02-16 20:33:41 +0100

    settings

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,5 +1,55 @@
-const Profile = () => {
-  return <div></div>;
+const userData = {
+  username: "Jacques Gluke",
+  tag: "jgluke",
+  location: "Ocho Rios, Jamaica",
+  avatar: "https://cdn-icons-png.flaticon.com/512/2922/2922506.png",
+  stats: {
+    followers: 5603,
+    views: 4827,
+    likes: 1308,
+  },
 };
 
-export default Profile;
+const Profile = (userData) => {
+  return (
+    <div>
+      <div>
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
+          alt="User avatar"
+        />
+        <p>Petra Marica</p>
+        <p>@pmarica</p>
+        <p>Salvador, Brasil</p>
+      </div>
+
+      <ul>
+        <li>
+          <span>Followers</span> <span>1000</span>
+        </li>
+        <li>
+          <span>Views</span> <span>2000</span>
+        </li>
+        <li>
+          <span>Likes</span> <span>3000</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+function App() {
+  return (
+    <div>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+    </div>
+  );
+}
+
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 // import App from "./components/App";
-import Profile from "./components/Profile";
+import App from "./components/Profile";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
